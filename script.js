f83db3cb7c0555a5f2daa8b4d333e2f4bd3ba5be
Thiago Rocha <124720719+thiago841@users.noreply.github.com>
2024-05-29 14:42:03 -0300

    Pagina Final

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
 //Tarea Clase 10
-let nombre= 'Thiago';
-let apellido= 'Rocha';
+// let nombre= 'Thiago';
+// let apellido= 'Rocha';
 
-console.log(nombre+' '+apellido)
+// console.log(nombre+' '+apellido)
 
 // let numerouno= 7;
 // let numerodos= 13;
@@ -51,58 +51,75 @@ console.log(nombre+' '+apellido)
 // }
 
 //TAREA CLASE 11: Ejemplo IF
-let topping="Oreo";
-let precio=0;
-let precioTotal=0;
-let helado= 50;
+// let topping="Oreo";
+// let precio=0;
+// let precioTotal=0;
+// let helado= 50;
 
-if (topping == "Oreo") {
-    precio= 10;
-} else if (topping == "KitKat") {
-    precio= 15;
-} else if (topping == "Kinder") {
-    precio= 25;
-} else {
-    precio=0;
-    console.log("No tenemos este topping");
+// if (topping == "Oreo") {
+//     precio= 10;
+// } else if (topping == "KitKat") {
+//     precio= 15;
+// } else if (topping == "Kinder") {
+//     precio= 25;
+// } else {
+//     precio=0;
+//     console.log("No tenemos este topping");
+// }
+
+// precioTotal= precio + helado;
+// console.log ("El helado cuesta $", precioTotal);
+
+// //Tarea 11: Ejemplo SWITCH
+// let menu= "Pescado";
+// switch (menu) {
+//     case "Carne":
+//     console.log("Vino tinto");
+//     break;
+//     case "Pescado":
+//     console.log("Vino blanco");
+//     break;
+//     case "Verdura":
+//     console.log("Agua");
+//     break;
+//     default:
+//     console.log("Elija carne, pescado o verdura");
+// }
+
+// //Tarea 11: Ejemplo FOR
+// let deportes= ["Futbol", "Basquet", "Tenis", "Padel"];
+// deportes.push("Boxeo");
+// for(let a=0; a<deportes.length; a++ ){
+//     console.log(deportes[a]);
+// }
+
+// //TAREA CLASE 12
+// let nom = prompt ("Ingrese su nombre")
+// function Saludo() {
+//     if (nom) {
+//         alert ("Bienvenido"+" "+ nom)
+//     }
+//     else {
+//         alert (" Debe ingresar un nombre");
+// }
+// }
+// Saludo();
+
+//PAGINA POLOS
+
+function mayorEdad(){
+    const  mayor = document.getElementById("ocultar");
+    mayor.style.display = "none";
 }
 
-precioTotal= precio + helado;
-console.log ("El helado cuesta $", precioTotal);
-
-//Tarea 11: Ejemplo SWITCH
-let menu= "Pescado";
-switch (menu) {
-    case "Carne":
-    console.log("Vino tinto");
-    break;
-    case "Pescado":
-    console.log("Vino blanco");
-    break;
-    case "Verdura":
-    console.log("Agua");
-    break;
-    default:
-    console.log("Elija carne, pescado o verdura");
+function menorEdad(){
+    alert("No puedes ingresar, debes ser mayor de 18 años");
 }
 
-//Tarea 11: Ejemplo FOR
-let deportes= ["Futbol", "Basquet", "Tenis", "Padel"];
-deportes.push("Boxeo");
-for(let a=0; a<deportes.length; a++ ){
-    console.log(deportes[a]);
+function enviarForm(){
+    alert("¡En breve nos estaremos comunicando!");
 }
 
-//TAREA CLASE 12
-let nom = prompt ("Ingrese su nombre")
-function Saludo() {
-    if (nom) {
-        alert ("Bienvenido"+" "+ nom)
-    }
-    else {
-        alert (" Debe ingresar un nombre");
-}
-}
-Saludo();
+
 
 
